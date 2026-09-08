0c2a894a61c431c679b3e9100e173c36901e4a5c
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-09-08 20:37:07 +0600

    Refactor App component: replace count state with second state for a timer implementation using CleanUp function.

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -3,18 +3,21 @@ import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [anotherCount, setAnotherCount] = useState(0);
+const [second, setSecond]= useState(0)
 
   useEffect(() => {
-    console.log('Use effect ran')
+  const timerId= setInterval(()=> {
+    setSecond((prev)=> prev+1)
 
-  }, [count]);
+   },1000) //1000 means 1000ms means 1 second
+
+   return ()=>clearInterval(timerId); // call back function return using CleanUp function
+
+  }, []);
 
   return (
     <>
-    <button onClick={()=> setCount(count + 1)}>Increment ({count})</button>
-    <button onClick={()=> setAnotherCount(anotherCount + 1)}>Increment Another ({anotherCount})</button>
+    <h1>{second}</h1>
     </>
   )
 
