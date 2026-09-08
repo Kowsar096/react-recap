b1c77bc0e8f4637b415b04bfc2aa3ea0cd04d5f4
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-09-08 18:52:53 +0600

    Understanding side effects and useEffect hook

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,10 +1,21 @@
 
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
- 
+  const [count, setCount] = useState(0);
+  const [anotherCount, setAnotherCount] = useState(0);
+
+  useEffect(() => {
+    console.log('Use effect ran')
+
+  }, [count]);
+
   return (
-   <h1>Hello</h1>
+    <>
+    <button onClick={()=> setCount(count + 1)}>Increment ({count})</button>
+    <button onClick={()=> setAnotherCount(anotherCount + 1)}>Increment Another ({anotherCount})</button>
+    </>
   )
 
 }
